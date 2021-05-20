26b28286fa695dc2057d9269cc90464ad39316a3
blueswizzle <83927505+blueswizzle@users.noreply.github.com>
2021-05-20 07:45:01 -0400

    Modified enrollStudentOption method in UI class by adding a while loop Fixed addTeacher method in UI class

--- src/database/UI.java
+++ src/database/UI.java
@@ -20,36 +20,38 @@ public class UI {
     public void start() throws FileNotFoundException {
         addCourses();
         addStudents();
-
-
+        addTeachers();
+        enrollStudentOption();
 
 
     }
     public void enrollStudentOption(){
-        int input = Integer.valueOf(scanner.nextLine());
-        switch(input){
-            case 1:
                 System.out.println("Which student?");
                 String student = scanner.nextLine();
-                if(school.studentExist(student)){
-                    System.out.println("Which course(s) should " + school.getStudentName(student) + " be enrolled in?");
-                    printCourses();
-                    int courseInput = Integer.valueOf(scanner.nextLine());
-                    switch(courseInput){
-                        case 1: school.getStudent(student).enroll("Physics", 300);
-                            break;
-                        case 2: school.getStudent(student).enroll("Calculus", 330);
-                            break;
-                        case 3: school.getStudent(student).enroll("English", 300);
-                            break;
-                        case 4: school.getStudent(student).enroll("Chemistry", 330);
-                            break;
-                        case 5: school.getStudent(student).enroll("Biology", 300);
-                            break;
+                while(true){
+                    if(school.studentExist(student)){
+                        System.out.println("Which course(s) should " + school.getStudentName(student) + " be enrolled in? Press 0 to end");
+                        printCourses();
+                        int courseInput = Integer.valueOf(scanner.nextLine());
+                        switch(courseInput){
+                            case 0: break;
+                            case 1: school.getStudent(student).enroll("Physics", 300);
+                                continue;
+                            case 2: school.getStudent(student).enroll("Calculus", 330);
+                                continue;
+                            case 3: school.getStudent(student).enroll("English", 300);
+                                continue;
+                            case 4: school.getStudent(student).enroll("Chemistry", 330);
+                                continue;
+                            case 5: school.getStudent(student).enroll("Biology", 300);
+                                continue;
+                        }
                     }
+                    break;
                 }
-        }
+
     }
+
     public void addCourses() throws FileNotFoundException {
         File coursesList = new File("/Users/atheek_99/Documents/Java Projects/School Database System/courselist.txt");
         Scanner fileReader = new Scanner(coursesList);
@@ -93,7 +95,7 @@ public class UI {
             String firstName = parts[0];
             String lastName = parts[1];
             String id = parts[2];
-            int salary = Integer.valueOf(parts[4]);
+            int salary = Integer.valueOf(parts[3]);
             school.addTeacher(new Teacher(firstName,lastName,id,salary));
         }
     }
